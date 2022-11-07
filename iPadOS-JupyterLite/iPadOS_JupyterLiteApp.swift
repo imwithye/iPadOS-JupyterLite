41ddf8bb977c37a7266755dfed30919581799f72
Yiwei Gong <imwithye@gmail.com>
2022-11-07 16:54:40 +0800

    Initial Commit

---
+++ iPadOS-JupyterLite/iPadOS_JupyterLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPadOS_JupyterLiteApp.swift
+//  iPadOS-JupyterLite
+//
+//  Created by Yiwei Gong on 2022/11/7.
+//
+
+import SwiftUI
+
+@main
+struct iPadOS_JupyterLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
